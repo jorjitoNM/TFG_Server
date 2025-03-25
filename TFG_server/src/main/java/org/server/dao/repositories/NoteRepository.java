d92ee7e92bda6892b957340d814d3b75c5cc1f90
MarioValiente2 <mario.valiente2@educa.madrid.org>
2025-03-25 20:51:23 +0100

    Minimo para empezar cada uno con sus tareas

---
+++ TFG_server/src/main/java/org/server/dao/repositories/NoteRepository.java
@@ -0,0 +1,9 @@
+package org.server.dao.repositories;
+
+import org.server.dao.model.note.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Integer> {
+}
