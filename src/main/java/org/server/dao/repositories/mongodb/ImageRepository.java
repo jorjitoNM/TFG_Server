1d5bc54cba5a5f9618457e15b95e54ecdfab12d6
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-04-17 23:25:36 +0200

    Creado metodo saveNoteImage. Comanzando con el getNoteImages.

--- src/main/java/org/server/dao/repositories/mongodb/ImageRepository.java
+++ src/main/java/org/server/dao/repositories/mongodb/ImageRepository.java
@@ -5,7 +5,10 @@ import org.server.dao.model.Image;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageRepository extends MongoRepository<Image, ObjectId> {
 
+    List<Image> findAllByNoteId (int noteId);
 }
