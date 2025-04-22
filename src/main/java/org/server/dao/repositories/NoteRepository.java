e18a2e9615f772d822ec6cb1606cfdfd7b9fb22c
cronoxxxx <saavedra.mateo.walter@gmail.com>
2025-04-22 11:20:48 +0200

    Trayendo cambios de feature-filtrarLikes a develop (cronoxxxx)

--- src/main/java/org/server/dao/repositories/NoteRepository.java
+++ src/main/java/org/server/dao/repositories/NoteRepository.java
@@ -2,6 +2,7 @@ package org.server.dao.repositories;
 
 import org.server.dao.model.note.Note;
 import org.server.dao.model.note.NoteType;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,9 +13,6 @@ import java.util.Optional;
 
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Integer> {
-    List<Note> findAllByOrderByLikesDesc();
-
-    List<Note> findAllByOrderByLikesAsc();
 
     @Query("SELECT n FROM Note n WHERE n.latitude = :latitude AND n.longitude= :longitude AND n.privacy = 'PUBLIC'")
     List<Note> findNotesByGeographicArea(@Param("latitude") double latitude, @Param("longitude") double longitude);
