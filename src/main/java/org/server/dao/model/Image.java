fa60e99918f9770171db535df78211c8e2d6c7b2
jorjitoNM <jorge.novillo@educa.madrid.org>
2025-05-10 21:39:58 +0200

    - Eliminado repositorio mongo - Limpiado el POM de librerias que no se usaban - Arregaldos imports - Arreglado error en likeNote, que duplicaba las filas

--- src/main/java/org/server/dao/model/Image.java
+++ src/main/java/org/server/dao/model/Image.java
@@ -1,25 +1,4 @@
 package org.server.dao.model;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.bson.types.Binary;
-import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "images")
-@AllArgsConstructor
-@NoArgsConstructor
 public class Image {
-    @Id
-    private ObjectId imageId;
-    private int noteId;
-    private Binary image;
-    private String imageType;
-
-    public Image(int noteId, Binary image, String imageType) {
-        this.noteId = noteId;
-        this.image = image;
-        this.imageType = imageType;
-    }
 }
