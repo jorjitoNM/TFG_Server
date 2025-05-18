5500271d1b72b72167cd9c7a86be43ee5b521cea
Adrian Saavedra <153601542+cronoxxxx@users.noreply.github.com>
2025-05-18 11:51:57 +0200

    Conflictos resueltos

--- src/main/java/org/server/domain/errors/ApiError.java
+++ src/main/java/org/server/domain/errors/ApiError.java
@@ -3,20 +3,9 @@ package org.server.domain.errors;
 
 import java.time.LocalDate;
 
-public class ApiError extends RuntimeException {
-    protected int code;
+public class ApiError  {
     protected String message;
-    protected LocalDate time;
-
-    public ApiError(int code, String message) {
-        this.code = code;
-        this.message = message;
-        this.time = LocalDate.now();
-    }
-
     public ApiError(String message) {
-        this.code = 500;
         this.message = message;
-        this.time = LocalDate.now();
     }
 }
